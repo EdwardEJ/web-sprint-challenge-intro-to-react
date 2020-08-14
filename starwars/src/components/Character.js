b527d2edf6c80bc2e669ce2c9fcfee0e60049e86
A <jesus.e.estrada@hotmail.com>
2020-08-14 12:55:21 -0500

    added animation

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const StyledCharContainer = styled.div`
  font-size: 1rem;
@@ -20,19 +20,33 @@ const StyledDetails = styled.p`
     }
 `
 
+const kf = keyframes`
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
+
+const StyledIntro = styled.div`
+  transform: rotateZ(180deg);
+  animation: ${kf} 0.5s ease-in-out forwards;
+`
+
 const Character = (props) => {
 
   const { info } = props;
 
   return (
     <StyledCharContainer>
-      <StyledName>{info.name}</StyledName>
-      <StyledDetails>Gender: {info.gender}</StyledDetails>
-      <p>Birth Year: {info.birth_year}</p>
-      <p>Eye Color: {info.eye_color}</p>
-      <p>Hair Color: {info.hair_color}</p>
-      <p>Height: {info.height}</p>
-      <p>Skin Color: {info.skin_color}</p>
+      <StyledIntro>
+        <StyledName>{info.name}</StyledName>
+        <StyledDetails>Gender: {info.gender}</StyledDetails>
+        <p>Birth Year: {info.birth_year}</p>
+        <p>Eye Color: {info.eye_color}</p>
+        <p>Hair Color: {info.hair_color}</p>
+        <p>Height: {info.height}</p>
+        <p>Skin Color: {info.skin_color}</p>
+      </StyledIntro>
     </StyledCharContainer>
   )
 }
