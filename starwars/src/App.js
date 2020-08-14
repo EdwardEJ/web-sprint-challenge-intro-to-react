4b08518e9d2d8a7a58b316c173049179f1c564e8
A <jesus.e.estrada@hotmail.com>
2020-08-14 10:35:22 -0500

    created axios get

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+
+import BASE_URL from './constants'
+
 
 const App = () => {
+  const [characterInfo, setCharacterInfo] = useState([]);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+      .then(res => {
+        debugger
+        setCharacterInfo(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+
+      }
     </div>
   );
 }
