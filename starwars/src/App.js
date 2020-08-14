0efc63fb13b3841d0ec6af6013a7e4aba19f0098
A <jesus.e.estrada@hotmail.com>
2020-08-14 12:41:59 -0500

    rendered characters to DOM

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,7 +9,7 @@ import Character from './components/Character'
 
 const App = () => {
   const [characterInfo, setCharacterInfo] = useState([]);
-  const [currentCharId, setCurrentCharId] = useState(null)
+  // const [currentCharId, setCurrentCharId] = useState(null)
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -17,13 +17,13 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const openDetails = birth_year => {
-    setCurrentCharId(birth_year)
-  }
+  // const openDetails = birth_year => {
+  //   setCurrentCharId(birth_year)
+  // }
 
-  const closeDetails = () => {
-    setCurrentCharId(null)
-  }
+  // const closeDetails = () => {
+  //   setCurrentCharId(null)
+  // }
 
   useEffect(() => {
     axios.get(`${BASE_URL}`)
@@ -35,33 +35,21 @@ const App = () => {
       })
   }, [])
 
-  const Characters = (props) => {
-
-    const { info } = props;
-
-    return (
-      <div className="charContainer">
-        {info.name}
-        <button onClick={() => openDetails(info.birth_year)}>{info.birth_year}</button>
-      </div>
-    )
-  }
-
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
       {
         characterInfo.map((char, indx) => {
-          return <Characters key={indx} info={char} openDetails={openDetails} />
+          return <Character key={indx} info={char} />
         })
       }
-      {
+      {/* {
         currentCharId && <Character
           charId={currentCharId}
           close={closeDetails}
         />
-      }
+      } */}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharContainer = styled.div`
+ font-size: 1rem;
+ border: 3px solid black;
+ border-radius: 2%;
+ width: 70%;
+ margin: auto;
+`
+const StyledName = styled.p`
+  color: white;
+  font-size: 2rem;
+`
+
+const StyledDetails = styled.div`
+  p: {
+    font-weight: bold;
+  }
+`
 
 const Character = (props) => {
-  const { close, info } = props;
 
-  console.log(info)
+  const { info } = props;
+
   return (
-    <div className='detailsContainer'>
-      {
+    <StyledCharContainer>
+      <StyledName>{info.name}</StyledName>
+      <StyledDetails>
         <p>Gender: {info.gender}</p>
-      }
-      <button onClick={close}>Close</button>
-    </div>
+        <p>Birth Year: {info.birth_year}</p>
+        <p>Eye Color: {info.eye_color}</p>
+        <p>Hair Color: {info.hair_color}</p>
+        <p>Height: {info.height}</p>
+        <p>Skin Color: {info.skin_color}</p>
+      </StyledDetails>
+    </StyledCharContainer>
   )
-
 }
 
 export default Character
