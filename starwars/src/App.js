0f115645204b5d0d9c19ca83d27b7fc6a2330124
A <jesus.e.estrada@hotmail.com>
2020-08-14 11:36:25 -0500

    rendered character names to DOM

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,9 +4,13 @@ import axios from 'axios'
 
 import BASE_URL from './constants'
 
+import Character from './components/Character'
+import Details from './components/Details'
+
 
 const App = () => {
   const [characterInfo, setCharacterInfo] = useState([]);
+  const [currentCharId, setCurrentCharId] = useState(null)
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -14,10 +18,18 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const openDetails = id => {
+    setCurrentCharId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharId(null)
+  }
+
   useEffect(() => {
     axios.get(`${BASE_URL}`)
       .then(res => {
-        debugger
         setCharacterInfo(res.data.results)
       })
       .catch(err => {
@@ -25,11 +37,18 @@ const App = () => {
       })
   }, [])
 
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
       {
-
+        characterInfo.map((char, indx) => {
+          return <Character key={indx} info={char} />
+        })
+      }
+      {
+        // currentCharId && <Details charId={currentCharId} close={closeDetails} />
       }
     </div>
   );
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from 'react'
+
+const Character = (props) => {
+
+  const { info } = props;
+
+  return (
+    <div className="charContainer">
+      {info.name}
+      <button >{info.birth_year}</button>
+    </div>
+  )
+}
+
+export default Character
