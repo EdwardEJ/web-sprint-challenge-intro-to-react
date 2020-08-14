fc8db308375723a331c08b70e208e232604cef26
A <jesus.e.estrada@hotmail.com>
2020-08-14 12:46:28 -0500

    added styled-components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -13,10 +13,11 @@ const StyledName = styled.p`
   font-size: 2rem;
 `
 
-const StyledDetails = styled.div`
-  p: {
+const StyledDetails = styled.p`
     font-weight: bold;
-  }
+    &:hover{
+      color: blue;
+    }
 `
 
 const Character = (props) => {
@@ -26,14 +27,12 @@ const Character = (props) => {
   return (
     <StyledCharContainer>
       <StyledName>{info.name}</StyledName>
-      <StyledDetails>
-        <p>Gender: {info.gender}</p>
-        <p>Birth Year: {info.birth_year}</p>
-        <p>Eye Color: {info.eye_color}</p>
-        <p>Hair Color: {info.hair_color}</p>
-        <p>Height: {info.height}</p>
-        <p>Skin Color: {info.skin_color}</p>
-      </StyledDetails>
+      <StyledDetails>Gender: {info.gender}</StyledDetails>
+      <p>Birth Year: {info.birth_year}</p>
+      <p>Eye Color: {info.eye_color}</p>
+      <p>Hair Color: {info.hair_color}</p>
+      <p>Height: {info.height}</p>
+      <p>Skin Color: {info.skin_color}</p>
     </StyledCharContainer>
   )
 }
